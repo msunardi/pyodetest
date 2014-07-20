aa14a1bc0fc9ec0a32daa79214a223c96c070e9c
msunardi <misunardi@hotmail.com>
2014-07-19 18:43:17 -0700

    Implemented moveable view

--- SimulationTest/glode.py
+++ SimulationTest/glode.py
@@ -40,7 +40,8 @@ def onKey(c, x, y):
 def onDraw():
     """ GLUT render callback """
 
-    prepare_GL()
+    #prepare_GL()
+    display()
     for b in bodies:
         draw_body(b)
     for b in ragdoll.bodies:
@@ -120,6 +121,167 @@ def init_GLUT():
     glutDisplayFunc(onDraw)
     glutIdleFunc(onIdle)
 
+g_fViewDistance = 9.
+g_Width = 800
+g_Height = 600
+
+g_nearPlane = 1.
+g_farPlane = 1000.
+
+action = ""
+xStart = yStart = 0.
+zoom = 65.
+
+xRotate = 0.
+yRotate = 0.
+zRotate = 0.
+
+xTrans = 0.
+yTrans = 0.
+
+
+
+def init():
+    glEnable(GL_NORMALIZE)
+    glLightfv(GL_LIGHT0,GL_POSITION,[ .0, 10.0, 10., 0. ] )
+    glLightfv(GL_LIGHT0,GL_AMBIENT,[ .0, .0, .0, 1.0 ]);
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,[ 1.0, 1.0, 1.0, 1.0 ]);
+    glLightfv(GL_LIGHT0,GL_SPECULAR,[ 1.0, 1.0, 1.0, 1.0 ]);
+    glEnable(GL_LIGHT0)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LESS)
+    glShadeModel(GL_SMOOTH)
+    resetView()
+
+"""
+def scenemodel():
+    glRotate(90, 0., 0., 1.)
+    glutSolidTeapot(1.)
+"""
+
+def resetView():
+    global zoom, xRotate, yRotate, zRotate, xTrans, yTrans
+    zoom = 65.
+    xRotate = 0.
+    yRotate = 0.
+    zRotate = 0.
+    xTrans = 0.
+    yTrans = 0.
+    glutPostRedisplay()
+
+def display():
+    # Clear frame buffer and depth buffer
+    #glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    #glClearColor(0.8, 0.8, 0.9, 0.0)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_NORMALIZE)
+    glShadeModel(GL_SMOOTH)
+    # Set up viewing transformation, looking down -Z axis
+    glLoadIdentity()
+    #gluLookAt(0, 0, -g_fViewDistance, 0, 0, 0, -.1, 0, 0)   #-.1,0,0
+    gluLookAt(1.5, 4.0, 3.0, 0.5, 1.0, 0.0, 0.0, 1.5, 0.0) 
+    # Set perspective (also zoom)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(zoom, float(g_Width)/float(g_Height), g_nearPlane, g_farPlane)
+    glMatrixMode(GL_MODELVIEW)
+    # Render the scene
+    polarView()
+    #scenemodel()
+    # Make sure changes appear onscreen
+    #glutSwapBuffers()
+
+def reshape(width, height):
+    global g_Width, g_Height
+    g_Width = width
+    g_Height = height
+    glViewport(0, 0, g_Width, g_Height)
+    
+
+def polarView():
+    glTranslatef( yTrans/100., 0.0, 0.0 )
+    glTranslatef(  0.0, -xTrans/100., 0.0)
+    glRotatef( -zRotate, 0.0, 0.0, 1.0)
+    glRotatef( -xRotate, 1.0, 0.0, 0.0)
+    glRotatef( -yRotate, .0, 1.0, 0.0)
+   
+def keyboard(key, x, y):
+    """  GLUT keyboard callback """
+    global zTr, yTr, xTr, SloMo, Paused
+    if(key=='r'): resetView()
+    if(key=='q' or key == 'Q'): sys.exit(0)    
+    
+    # Set simulation speed
+    if key >= '0' and key <= '9':
+        SloMo = 4 * int(key) + 1
+    # Pause/unpause simulation
+    if key == 'p' or key == 'P':
+        Paused = not Paused
+        print "Paused: %s" % (Paused)
+    
+    glutPostRedisplay()
+
+
+def mouse(button, state, x, y):
+    global action, xStart, yStart
+    if (button==GLUT_LEFT_BUTTON):
+        if (glutGetModifiers() == GLUT_ACTIVE_SHIFT):
+            action = "MOVE_EYE_2"
+        else:
+            action = "MOVE_EYE"
+    elif (button==GLUT_MIDDLE_BUTTON):
+        action = "TRANS"
+    elif (button==GLUT_RIGHT_BUTTON):
+        action = "ZOOM"
+    xStart = x
+    yStart = y
+
+
+def motion(x, y):
+    global zoom, xStart, yStart, xRotate, yRotate, zRotate, xTrans, yTrans
+    if (action=="MOVE_EYE"):
+        xRotate += x - xStart
+        yRotate -= y - yStart
+    elif (action=="MOVE_EYE_2"):
+        zRotate += y - yStart
+    elif (action=="TRANS"):
+        xTrans += x - xStart
+        yTrans += y - yStart
+    elif (action=="ZOOM"):
+        zoom -= y - yStart
+        if zoom > 150.:
+            zoom = 150.
+        elif zoom < 1.1:
+            zoom = 1.1
+    else:
+        print("unknown action\n", action)
+    xStart = x
+    yStart = y 
+    glutPostRedisplay()
+
+def glSetup():
+    # GLUT Window Initialization
+    glutInit()
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB| GLUT_DEPTH)      # zBuffer
+    glutInitWindowSize (g_Width,g_Height) 
+    glutInitWindowPosition (0 + 4, g_Height / 4)
+    glutCreateWindow ("GLODE View")
+    # Initialize OpenGL graphics state
+    init ()
+    # Register callbacks
+    glutReshapeFunc(reshape)
+    #glutDisplayFunc(display)    
+    glutDisplayFunc(onDraw)
+    glutMouseFunc(mouse)
+    glutMotionFunc(motion)
+    glutKeyboardFunc(keyboard)
+    glutIdleFunc(onIdle)
+    #printHelp()
+    glutMainLoop()
+
 # Create an ODE world object
 world = ode.World()
 world.setGravity((0.0, -9.81, 0.0))
@@ -166,8 +328,9 @@ bodies.append(obstacle)
 print "obstacle created at %s" % (str(pos))
 
 def main():
-    init_GLUT()
+    #init_GLUT()
+    glSetup()
     # Enter the GLUT event loop
-    glutMainLoop()
+    #glutMainLoop()
 #exit()
 
